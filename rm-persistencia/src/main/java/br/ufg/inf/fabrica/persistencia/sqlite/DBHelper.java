6a2f8fc69a74b0b924c4d6c3865a18c0eca4777f
kaiomagalhaes <contato@kaiomagalhaes.com.br>
2015-06-14 15:32:54 -0300

    creating dbhelper

---
+++ rm-persistencia/src/main/java/br/ufg/inf/fabrica/persistencia/sqlite/DBHelper.java
@@ -0,0 +1,19 @@
+package br.ufg.inf.fabrica.persistencia.sqlite;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBHelper {
+
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            DriverManager.getConnection("jdbc:sqlite:rmpersistencia.sqlite3");
+        } catch (Exception e) {
+            System.out.println(e.getStackTrace());
+        }
+        return conn;
+    }
+
+}
